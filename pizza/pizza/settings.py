c502eddbde2f6d46efcab05e3666132d9973b7f8
thompm24 <mark.thompson24@mail.dcu.ie>
2024-02-08 22:28:34 +0000

    Sass installed?

--- pizza/pizza/settings.py
+++ pizza/pizza/settings.py
@@ -121,6 +121,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+SASS_PROCESSOR_ROOT = STATIC_ROOT
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
