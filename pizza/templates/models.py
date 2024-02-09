e8e1604fed74bee8269b9031135fe4aa7f0edec6
thompm24 <mark.thompson24@mail.dcu.ie>
2024-02-09 15:11:37 +0000

    Made all_pizzas.html and _all_pizzas.scss

--- pizza/templates/models.py
+++ pizza/templates/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+  id=models.SmallAutoField(primary_key=True)
+  name = models.CharField(max_length=10)
+  price_multiplier = models.DecimalField(max_digits=3, decimal_places=2, default=1)
+
+class Pizza(models.Model):
+  id=models.SmallAutoField(primary_key=True)
+  name = models.CharField(max_length=30)
+  price = models.DecimalField(max_digits=4, decimal_places=2, default=12)
+  image = models.ImageField(upload_to='images/')
--- pizza/templates/views.py
+++ pizza/templates/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
 	return render(request, 'index.html')
+
+def pizza(request, pizza_id):
+	pizza =Pizza.objects.get(pk=pizza_id)
+	if pizza is not None:
+		return render(request, 'pizza.html', {'pizza': pizza})
+	else:
+		return Http404('Pizza does not exist')
+
+def all_pizzas(request):
+	all_pizzas = Pizza.objects.all()
+	return render(request, 'all_pizzas.html', {'pizzas':all_pizzas})
